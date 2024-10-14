501da481bdd671ce80203e2ba1ba489d93a8d9d4
arturmart <artur.mart2002@gmail.com>
2024-10-14 14:32:40 +0400

    Create lab3_1.py

---
+++ Python/lab3_1.py
@@ -0,0 +1,24 @@
+arr = "hello world"
+
+varA = "aeiouh"
+varB = "bcdfgjklmnpqrstvwxz"
+
+def findArr(char,arr):
+    for i in arr:
+        if (i == char):
+            return True
+    return False
+
+A = 0
+B = 0
+
+arr.lower()
+for i in arr:
+    if(findArr(i,varA)):
+        A+=1
+    if(findArr(i,varB)):
+        B+=1
+
+    
+print(A)
+print(B)

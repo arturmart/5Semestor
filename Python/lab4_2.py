9a450a7483599b3654c9625734e9ae4cd234ec03
arturmart <artur.mart2002@gmail.com>
2024-10-16 23:01:24 +0400

    Update lab4_2.py

--- Python/lab4_2.py
+++ Python/lab4_2.py
@@ -4,15 +4,8 @@ brr2 = {"B": 4, "D" : 5, "E" : 6}
 
 print()
 
-    
-for i in brr1:
-    print(i ," " , brr1[i])
-    
-    if(brr2.get(i)):
-        d = brr1[i]+brr2[i]
-        brr2.update(i, d)
-    if(not brr2.get(i)):
-        brr2[i]= brr1[i]
+print(brr1.keys() | brr2.keys())
+print(brr1.update(brr2))
 
 
-print(brr2)
+print(brr1)

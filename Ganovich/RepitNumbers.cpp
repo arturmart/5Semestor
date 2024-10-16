95650cead2c38eda3efc169fe0855bb6d95c7c08
arturmart <artur.mart2002@gmail.com>
2024-10-16 22:34:34 +0400

    Update RepitNumbers.cpp

--- Ganovich/RepitNumbers.cpp
+++ Ganovich/RepitNumbers.cpp
@@ -1,39 +1,74 @@
-﻿
+
 #include <iostream>
 #include <vector>
 
 
 class RepitNumbers {
 public:
-    void clearRepitNumbers(std::vector<int>& arr) {
-        int buf = arr[0];
-        for (int i = 1; i < arr.size(); i++) {
-            if (buf == arr[i]) {
-                arr.erase(arr.begin() + i);
-                i--;
-                
-            }
-            else {
-                buf = arr[i];
-                
-            }
+    int clearRepitNumbers(std::vector<int>& arr) {
+        if(!isValidSort(arr) || !isValidRepeat(arr))
+            throw std::invalid_argument("The array is invalid");
+
+        int j = 1;
+        for(int i = 1; i < arr.size(); i++){
+            if(arr[i] != arr[i - 1]) {
+                arr[j] = arr[i];
+                j++;
+            }   
         }
+        return j;
     }
-    void printArr(std::vector<int> arr) {
-        for (int i = 0; i < arr.size(); i++) {
-            std::cout << arr[i] << "\t";
+    void printArr(std::vector<int> arr, int size) {
+        for (int i = 0; i < size; i++) {
+            std::cout << arr[i] << " ";
         }
         std::cout << std::endl;
     }
+    private:
+
+    bool isValidSort(std::vector<int>& arr){
+
+        for(int i = 1; i < arr.size(); i++){
+            if(arr[i] < arr[i - 1]) {
+                return false;
+            }   
+        }
+        return true;
+
+    }
+    bool isValidRepeat(std::vector<int>& arr){
+
+
+        for(int i = 1; i < arr.size(); i++){
+            if(arr[i] == arr[i - 1]) {
+                return true;
+            }   
+        }
+        return false;
+
+    }
+
+
+
 };
 
 
 int main()
 {
     std::vector<int> arr = { 1,2,3,3,3,5,8,8,9,10,11,12,15,16,16,16,20,20,21,22,25,25,30 };
-    std::vector<int> arr2 = { 1,2,3};
+    std::vector<int> arr2 = { 4, 1,2,3,3 };
     RepitNumbers rp;
-    rp.clearRepitNumbers(arr);
-    rp.printArr(arr);
-}
+    int size = arr2.size();
+    try
+    {
+        size = rp.clearRepitNumbers(arr2);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+    
 
+    rp.printArr(arr2,size);
+}

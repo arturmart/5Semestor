b1300413797102856b1c551346d844af47f5d929
arturmart <artur.mart2002@gmail.com>
2024-10-16 23:21:02 +0400

    Update and rename lab4.py to lab2_1.py

--- Python/lab2_1.py
+++ Python/lab2_1.py
@@ -1,14 +1,23 @@
 print("num:")
 x = int(input())
+list = []
 
 
 for i in range(1,x):
     flag = True
+    
     for j in range(2,i):
         if(i%j==0):
             flag = False
+            print(j, end =" ")
+            
     if(flag):
-        print(i)
+        print("+", end =" ")
+        list.append(i)
+
+    print("(",i,")")
+
+print(list)
 
     
 

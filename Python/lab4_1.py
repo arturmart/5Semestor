700c357628fbdc43ebcf4cbf2168833dc89f7ced
arturmart <artur.mart2002@gmail.com>
2024-10-16 22:51:13 +0400

    Update lab4_1.py

--- Python/lab4_1.py
+++ Python/lab4_1.py
@@ -5,7 +5,8 @@ kort1 = (1,2,3,4,5,6,7,8,9,10)
 
 
 def kortFirstEnd(kort):
-    kort2 = (kort[len(kort)-1], *kort[1:len(kort)-2] ,kort[0])
+    kort2 = (kort[-1],) + kort[1:-2] + (kort[0],)
+    #kort2 = (kort[len(kort)-1], *kort[1:len(kort)-2] ,kort[0])
 
     return (kort2)
 

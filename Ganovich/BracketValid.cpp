e7469ff9d7e1e4f733aa06f3b26543ef2ae188e2
arturmart <artur.mart2002@gmail.com>
2024-10-20 18:01:02 +0400

    Create BracketValid.cpp

---
+++ Ganovich/BracketValid.cpp
@@ -0,0 +1,74 @@
+// pakagcer.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+const char BRACKET_OPEN[3] = { '(','[','{' };
+
+const char BRACKET_CLOSE[3] = {')', ']', '}'};
+
+
+class BracketValid {
+public:
+    BracketValid(){}
+
+    bool checkBracketValid(std::string& str) {
+
+        clearSrack();
+        
+        
+        for (int i = 0; i < str.size(); i++) {
+
+            for (int j = 0; j < sizeof(BRACKET_OPEN); j++) {
+                if (str[i] == BRACKET_OPEN[j]) {
+                    stk.push(j);
+                }
+                else if (str[i] == BRACKET_CLOSE[j]) {
+                    if (stk.top() == j) stk.pop();
+                    else return false;
+                    
+                }
+            }
+
+        }
+
+        if (stk.empty()) return(true);
+        
+        else return(false);
+
+
+
+
+    }
+private:
+
+    void clearSrack() {
+        while (!stk.empty()) {
+            stk.pop();
+        }
+    }
+
+    stack<int> stk;
+};
+
+
+
+int main()
+{
+    BracketValid BV;
+    std::string str;
+    std::cout << "Input Text: ";
+    std::cin >> str;
+
+    if (BV.checkBracketValid(str)) {
+        std::cout << "Text Brackets is Valid!\n";
+    }
+    else {
+        std::cout << "Text Brackets is Incorrect!\n";
+    }
+
+    
+}
+

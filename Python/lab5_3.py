73c3a1765d590fb52691ffe626a11d1247fbb5da
arturmart <artur.mart2002@gmail.com>
2024-11-11 15:28:11 +0400

    Add files via upload

---
+++ Python/lab5_3.py
@@ -0,0 +1,33 @@
+
+import math
+
+
+
+class Cotorak:
+    def __init__(self,hamarich,haytarar):
+        self.__hamarich = int(hamarich)
+        self.__haytarar = int(haytarar)
+
+    def getCotorak(self):
+        return (str(self.__hamarich)+"/"+str(self.__haytarar))
+    def getHaytarar(self):
+        return (__haytarar)
+    def getHamarich(self):
+        return (__hamarich) 
+
+    def __add__(self, other):
+        ts = Cotorak(0,0)
+        #gcd = math.gcd(self.getHaytarar(),other.getHaytarar())
+        print(gcd)
+     
+        
+
+
+
+
+x = Cotorak(1,2)
+y = Cotorak(1,3)
+
+x + y
+
+#print(z.getCotorak())
